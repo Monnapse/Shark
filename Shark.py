7ec789f3b266f6c5c1608f68eaa434974ace8a82
Monnapse <masonshuber@gmail.com>
2024-04-22 21:24:05 -0600

    Create Shark.py

---
+++ Shark.py
@@ -0,0 +1,51 @@
+"""
+    Turn your html code into one line ready for Shark Rendering or other ESP applications.
+    Made by Monnapse
+    0.1.0
+
+    python Shark.py Path/To/Your/File.html
+"""
+
+import sys
+
+commands = []
+
+def command(function):
+    commands.append(function)
+
+def r_cmd(args):
+    args.pop(0)
+    function_name = args.pop(0)
+
+    arguments = []
+    long_options = []
+
+    for arg in args:
+        if str.startswith(arg, "--"):
+            long_options.append(arg)
+        else:
+            arguments.append(arg)
+
+    for f in commands:
+        if f.__name__ == function_name:
+            
+            f(*tuple(arguments))
+            break
+
+def start():
+    r_cmd(sys.argv)
+
+@command
+def string(string: str):
+    new_string = string.replace('\n', '\\n').replace('\t', '\\t').replace('\r', '\\r').replace('"', '\\"')
+    print(new_string)
+    return new_string
+
+@command
+def path(path: str):
+    string = open(path, 'r').read()
+    new_string = string.replace('\n', '\\n').replace('\t', '\\t').replace('\r', '\\r').replace('"', '\\"')
+    print(new_string)
+    return new_string
+
+start()
